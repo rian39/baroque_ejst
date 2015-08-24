2c05fb8614d325585e5c000dd02bb91f33580357
rian39 <a.mackenzie@lancaster.ac.uk>
2015-08-24 12:35:24 +0100

    fixing jl queries on final formatted version

--- pandoc.sh
+++ pandoc.sh
@@ -7,6 +7,11 @@
 ##format bibliography and  display 
 #pandoc --smart --normalize --latex-engine=xelatex  --bibliography=/home/mackenza/ref_bibs/data_forms_thought.bib  --bibliography=/home/mackenza/ref_bibs/machine_learning.bib --bibliography=/home/mackenza/ref_bibs/R.bib --csl=references/sage-harvard.csl --template=/home/mackenza/template.latex mackenzie_mattering_march2015.md  -o mackenzie_mattering_march2015.pdf
 
-pandoc --smart --normalize --latex-engine=xelatex  --bibliography=/home/mackenza/ref_bibs/data_forms_thought.bib  --bibliography=/home/mackenza/ref_bibs/machine_learning.bib --bibliography=/home/mackenza/ref_bibs/R.bib --csl=references/sage-harvard.csl  mackenzie_mattering.md -o mackenzie_mattering.doc
-libreoffice mackenzie_mattering.doc
+#pandoc --smart --normalize --latex-engine=xelatex  --bibliography=/home/mackenza/ref_bibs/data_forms_thought.bib  --bibliography=/home/mackenza/ref_bibs/machine_learning.bib --bibliography=/home/mackenza/ref_bibs/R.bib --csl=references/sage-harvard.csl  mackenzie_mattering.md -o mackenzie_mattering.doc
+
+#libreoffice mackenzie_mattering.doc
+
+pandoc --smart --normalize --latex-engine=xelatex  --bibliography=/home/mackenza/ref_bibs/data_forms_thought.bib  --bibliography=/home/mackenza/ref_bibs/machine_learning.bib --bibliography=/home/mackenza/ref_bibs/R.bib --csl=references/sage-harvard.csl  mackenzie_mattering.md -o mackenzie_mattering.pdf
+
+evince mackenzie_mattering.pdf
 
